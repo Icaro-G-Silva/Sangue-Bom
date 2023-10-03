ac7dd1b5307af542d52b3e0f7c47d70ce5a8cc8c
icaro.silva <icaro.silva.ext>
2023-10-03 14:23:47 -0300

    Add backend first structure

---
+++ backend/src/main/kotlin/com/sangue/bom/SangueBom/SangueBomApplication.kt
@@ -0,0 +1,11 @@
+package com.sangue.bom.SangueBom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SangueBomApplication
+
+fun main(args: Array<String>) {
+	runApplication<SangueBomApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/sangue/bom/SangueBom/SangueBomApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sangue.bom.SangueBom
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SangueBomApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
